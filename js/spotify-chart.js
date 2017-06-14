cd73960cb5ec81ff03009246b79d9b8a878ed1c6
sochetanie <allagorik2901@gmail.com>
2017-06-13 22:07:51 -0400

    done

--- js/spotify-chart.js
+++ js/spotify-chart.js
@@ -17,31 +17,56 @@ $(function() {
 
 function extractTop10Tracks(tracks) {
   // your code here
+  return tracks.slice(0,10)
 }
 
 function extractPopularity(tracks) {
   // your code here
+  return tracks.map(track => { return track.popularity })
 }
 
 function extractNames(tracks) {
   // your code here
+  return tracks.map(track => { return track.name })
 }
 
 function chartData(labels, inputData) {
   // your code here
-
+  var data = {
+    labels: labels,
+    datasets: [
+      {
+        fillColor: 'rgba(220,220,220,0.5)', 
+        strokeColor: 'rgba(220,220,220,0.8)', 
+        highlightFill: 'rgba(220,220,220,0.75)', 
+        highlightStroke: 'rgba(220,220,220,1)',
+        data: inputData
+      }
+    ]
+  }
   // use the dataSetProperties variable defined above if it helps
+  return data
 }
 
 function getSpotifyTracks(callback){
   // your ajax call here, on success it should call on the 
   // parameter it's passed (it's a function), and pass it's 
   // parameter the data it received
-
+  $.ajax({
+    url: url,
+    success: function(result) {
+      callback(result)
+    }
+  })
   // use the url variable defined above if it helps
 }
 
 function success(parsedJSON) {
+  var tracks = extractTop10Tracks(parsedJSON.tracks)
+  var names = extractNames(tracks)
+  var popularity = extractPopularity(tracks)
+
+  var data = chartData(names, popularity);
   // this function will make a new bar chart, refer to this url:
   // http://www.chartjs.org/docs/#bar-chart
   // you will need to call on:
@@ -51,5 +76,7 @@ function success(parsedJSON) {
   //  4. chartData - pass it results of #2 and #3
   //  5. make a variable `ctx` and select the canvas with the id of spotify-chart
   //     * also make sure to specify 2d context
+  var ctx = $('#spotify-chart').get(0).getContext('2d')
+  var myBarChart = new Chart(ctx).Bar(data, dataSetProperties)
   //  6. make a new bar chart!
 }
